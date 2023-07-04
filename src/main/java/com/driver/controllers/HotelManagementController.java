974d32767b725e5b31a3ec0337034c0c1496f584
ritikbarmecha <ritikbarmecha@gmail.com>
2023-07-04 22:03:57 +0530

    ritk commit for you

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -20,7 +20,10 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
-
+ /**
+ *
+ */
+HotelManagementService hotelManagementService = new HotelManagementService();
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
 
@@ -29,8 +32,8 @@ public class HotelManagementController {
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
 
-
-        return null;
+      return hotelManagementService.addHotel(hotel);
+        
     }
 
     @PostMapping("/add-user")
@@ -39,7 +42,7 @@ public class HotelManagementController {
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
 
-       return null;
+       return hotelManagementService.addUser(user);
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -49,7 +52,8 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        return null;
+    return hotelManagementService.getHotelWithMostFacilities();                
+
     }
 
     @PostMapping("/book-a-room")
@@ -62,14 +66,14 @@ public class HotelManagementController {
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid 
         
-        return 0;
+        return hotelManagementService.bookRoom(booking);
     }
     
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person 
-        return 0;
+         return hotelManagementService.getBookings(aadharCard);
     }
 
     @PutMapping("/update-facilities")
@@ -79,7 +83,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return null;
+         return hotelManagementService.updateFacilities(newFacilities, hotelName);
     }
 
 }
